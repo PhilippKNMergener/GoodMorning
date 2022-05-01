5b2617b91edbeff708bda9ba45ef7b702917a28c
PhilippKNMergener <60490212+PhilippKNMergener@users.noreply.github.com>
2022-05-01 12:16:16 -0700

    Base Project Created

---
+++ GoodMorning/GoodMorning/GoodMorningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodMorningApp.swift
+//  GoodMorning
+//
+//  Created by Philipp Mergener on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodMorningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
