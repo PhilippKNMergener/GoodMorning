447ab7c9208ebc1df774940cb661d1c0906eefa7
PhilippKNMergener <60490212+PhilippKNMergener@users.noreply.github.com>
2022-05-27 11:19:50 -0700

    Created PromptView

--- GoodMorningV1/GoodMorningV1/ContentView.swift
+++ GoodMorningV1/GoodMorningV1/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
         }
         .onTapGesture {
             BGState.updateState()
-            
+            FGState.updateState()
         }
     }
 }
--- GoodMorningV1/GoodMorningV1/PromptView.swift
+++ GoodMorningV1/GoodMorningV1/PromptView.swift
@@ -7,13 +7,35 @@
 
 import SwiftUI
 
+struct PromptText: View {
+    let text: String
+    let isShowing: Bool
+    var body: some View {
+        Text(text)
+            .bold()
+            .italic()
+            .foregroundStyle(.regularMaterial)
+            .opacity(isShowing ? 1 : 0)
+    }
+}
+
 struct PromptView: View {
     
     @State private var viewState: Int = 0
+    var actionsCompleted: Bool {
+        viewState >= 4
+    }
     
     var body: some View {
-        VStack(alignment: 30) {
-            
+        ZStack {
+            VStack(alignment: .center, spacing: 20) {
+                PromptText(text: "How are you feeling?", isShowing: viewState >= 1)
+                PromptText(text: "What is your goal?", isShowing: viewState >= 2)
+                PromptText(text: "What will you achieve today?", isShowing: viewState >= 3)
+            }
+        }
+        .onTapGesture {
+            viewState += 1
         }
     }
 }
